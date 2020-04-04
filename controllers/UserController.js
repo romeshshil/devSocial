4d814cfe1ff0b7faceb1ab5d05158370b8d6add8
Romesh Shil <romeshshil99@gmail.com>
2020-04-04 18:50:12 +0600

    JWT and Avatar add Commit

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,67 @@
-const { validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { validationResult } = require('express-validator');
+const normalize = require('normalize-url');
 
+// model import 
+const User = require('./../models/User');
 
 exports.createUser = async(req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User Registation Route");
+
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res
+                .status(400)
+                .json({ errors: [{ msg: 'User already exists' }] });
+        }
+
+        const avatar = normalize(
+            gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            }), { forceHttps: true }
+        );
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'), { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            }
+        );
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
 }
