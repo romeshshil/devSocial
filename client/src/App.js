462a232e640018b91ffed7ef947fdf467e2019a7
Romesh Shil <romeshshil99@gmail.com>
2020-04-09 16:44:38 +0600

    frontent Update Commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+import './App.css';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+const App =()=>(
+  <Provider store={store}>
+  <Router>
+    <Fragment>
+      <Navbar></Navbar>
+      <Route exact path='/' component={Landing}></Route>
+      <section className="container">
+        <Alert></Alert>
+        <Switch>
+          <Route exact path='/login' component={Login}></Route>
+          <Route exact path='/register' component={Register}></Route>
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+  </Provider>
+);
+
+export default App;
